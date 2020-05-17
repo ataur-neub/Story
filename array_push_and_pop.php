03f732919bb52989a7dbc11a3968c35b61ef202c
Ataur Rahman Selim <37262722+ataur-neub@users.noreply.github.com>
2020-05-17 10:31:16 +0600

    complete array_pop() method

--- array_push_and_pop.php
+++ array_push_and_pop.php
@@ -5,6 +5,15 @@ array_push() and array_pop() both are built in method. array_push() method helps
     $newElement=array_push($example,"C");//here C will be inserted after javascript
     print_r($newElement);
     
-    echo $newElement ."\n";
+    echo $newElement ."\n";//here total array length will be displayed
+
+?>
+
+/* use of array_pop()*/
+<?php
+    $arrayPopExample=["we","love","programming","and"];
+    $popedElement=array_pop();//last element of the array will be removed
+print_r($popedElement);//
+
 
 ?>
