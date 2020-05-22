d089fe5842b196946ec05f37559de2c1d42d83f9
Ataur Rahman Selim <37262722+ataur-neub@users.noreply.github.com>
2020-05-22 11:20:05 +0600

    add output value

--- remove_duplicacy.php
+++ remove_duplicacy.php
@@ -1,7 +1,17 @@
 <?php
-  $data=[1,2,4,22,33,33,1,2,22];
-  print_r(array_unique($data));
-  
+  $data=[1,2,4,22,33,33,1,2,22];// here there are some duplicate value
+  print_r(array_unique($data));//array_unique() function helps to remove duplicated value
 
-
+ 
 ?>
+//output
+/*
+Array(
+[0]=>1
+[1]=>2
+[2]=>4
+[3]=>22
+[4]=>33
+
+)
+*/
